470b46638c1cef57a0530e3a6a7af6d78ffdf7cf
NASIF IQBAL <150609244+Nasif-Iqbal@users.noreply.github.com>
2025-01-16 08:46:10 +0600

    Update Insertion_sort.cpp

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,1 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n=5;
+    int arr[n+10]={21,1,10,5,2};
+
+    for(int i=1; i<n; i++)
+    {
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    return 0;
+}
 
