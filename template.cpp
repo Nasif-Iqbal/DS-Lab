c286c91741ac613a438c409ae427fbe338332735
NASIF IQBAL <150609244+Nasif-Iqbal@users.noreply.github.com>
2025-02-28 07:11:28 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define all(v) v.begin(),v.end()
+#define pb(x) push_back(x)
+#define pob(x) pop_back(x)
+//#define v vector<int>
+#define all(v) v.begin(),v.end()
+#define allr(v) v.rbegin(),v.rend()
+#define foro(n) for(int i=0;i<int(n);i++)
+#define for1(n) for(int i=1;i<=int(n);i++)
+#define fup(a,n) for(int i=a;i<int(n);i++)
+#define lo(a,n) lower_bound(all(a),n)-a.begin()
+#define up(a,n) upper_bound(all(a),n)-a.begin()
+#define big(v) *max_element(all(v))
+#define small(v) *min_element(all(v))
+#define tc int t;cin>>t;while(t--)
+#define Y cout << "YES" << endl
+#define N cout << "NO" << endl
+#define yn                 \
+    cout << "YES" << endl; \
+    else cout << "NO" << endl;
+#define om cout << "-1" << endl
+#define sz(x) (int)x.size()
+#define sp " "
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+void solve()
+{
+    tc
+    {
+
+    }
+}
+
+
+int main()
+{
+    optimize()
+
+    solve();
+
+    return 0;
+}
